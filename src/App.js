9d95e9e0cd6548984cd5e2c353f35bb2cd57496e
Kamil Mazelanik <kamilmaz9206@gmail.com>
2021-04-03 23:44:25 +0200

    add set a places

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
 
 import LocationFinder from './components/LocationFinder';
 
 function App() {
   return (
-    <MapContainer center={[51.758320, 19.462100]} zoom={10}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <LocationFinder />
-    </MapContainer>
+    <LocationFinder />
   );
 }
 
--- src/components/LocationFinder.js
+++ src/components/LocationFinder.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import ReactMapGL from 'react-map-gl';
 import Geocoder from 'react-mapbox-gl-geocoder';
 import { Container, Col, Row } from 'reactstrap';
-import './LocationFinder.css'
+
+const mapStyle = {
+  width: '100%',
+  height: 600
+}
+
 const mapboxApiKey = 'pk.eyJ1Ijoia2F3ZWlnaHQiLCJhIjoiY2tuMXNlMHZ2MHhuZzJwcXV5M2RheDcyaiJ9.KFqFmepfuGHIb7-wp_1blA'
 
 const params = {
   country: "pl"
 }
 
-class LocationFinder extends Component {
+class LocationFinder extends PureComponent {
+
   constructor(props) {
     super(props);
     this.state = {
       viewport: {
         latitude: 45.50884,
         longitude: -73.58781,
-        zoom: 10
+        zoom: 15
       }
-    }
+    };
+
   }
 
   onSelected = (viewport, item) => {
@@ -28,20 +36,33 @@ class LocationFinder extends Component {
 
   render() {
     const { viewport } = this.state;
-
     return (
-      <Row className="py-4">
-        <Col xs={2}>
-          <Geocoder
-            mapboxApiAccessToken={mapboxApiKey}
-            onSelected={this.onSelected}
-            viewport={viewport}
-            hideOnSelect={true}
-            value=""
-            queryParams={params}
-          />
-        </Col>
-      </Row>
+      <Container fluid={true}>
+        <Row className="py-4">
+          <Col xs={2}>
+            <Geocoder
+              mapboxApiAccessToken={mapboxApiKey}
+              onSelected={this.onSelected}
+              viewport={viewport}
+              hideOnSelect={true}
+              value=""
+              queryParams={params}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ReactMapGL
+              mapboxApiAccessToken={mapboxApiKey}
+              mapStyle="mapbox://styles/mapbox/streets-v11"
+              {...viewport}
+              {...mapStyle}
+              onViewportChange={(viewport) => this.setState({ viewport })}
+            >
+            </ReactMapGL>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
