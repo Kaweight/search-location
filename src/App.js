b671c6857a61f5c8be364dac17f5aa62e21d0f65
Kamil Mazelanik <kamilmaz9206@gmail.com>
2021-04-03 14:53:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+
+function App() {
+  return (
+    <Map></Map>
+  );
+}
+
+export default App;
